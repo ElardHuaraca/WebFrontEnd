0f6ecb92b9c162da0c7a79e32bfb0d9c5fff415c
Elard Huaraca <56555556+jajanotecreo@users.noreply.github.com>
2022-03-02 07:21:12 -0500

    Add Product model and implement product service

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,9 @@
+import { Optional } from "@angular/core";
+
+export class Product {
+    productID?: number;
+    productName?: string;
+    productInventory?: number;
+    productExpiration?: string;
+    productRegistered?: string;
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  url = 'http:///localhost:44342/api/Product';
+
+  constructor(private http: HttpClient) { }
+
+  getProductsActivate() {
+    return this.http.get(this.url + '/GetProductActivate');
+  }
+
+  getProductsDeactivate() {
+    return this.http.get(this.url + '/GetProductDesactivate');
+  }
+
+  postProduct(product: Product) {
+    return this.http.post(this.url + '/PostProduct', product);
+  }
+
+  putProduct(id: number, product: Product) {
+    return this.http.put(this.url + '/PutProduct/' + id, product);
+  }
+
+  putProductDesactivate(id: number) {
+    return this.http.put(this.url + '/PutProductDesactivate/' + id, null);
+  }
+
+  putProductActivate(id: number) {
+    return this.http.put(this.url + '/PutProductActivate/' + id, null);
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete(this.url + '/DeleteProduct/' + id);
+  }
+}
